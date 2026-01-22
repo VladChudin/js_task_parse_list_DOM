029d9842b540095198609e389d03d5042d29887d
Vlad Chudin <chudintttks@gmail.com>
2026-01-22 09:39:01 +0100

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,29 +6,26 @@ function getSalary(item) {
   return Number(item.dataset.salary.replace(/[$,]/g, ''));
 }
 
-function sortList(list) {
+function sortList() {
   const itemsArray = Array.from(list.children);
 
-  itemsArray.sort((a, b) => getSalary(a) - getSalary(b));
+  itemsArray.sort((a, b) => getSalary(b) - getSalary(a));
 
   list.innerHTML = '';
 
   itemsArray.forEach((item) => list.append(item));
 }
 
-function getEmployees(list) {
+function getEmployees() {
   const itemsArray = Array.from(list.children);
 
   return itemsArray.map((item) => ({
     name: item.textContent.trim(),
     position: item.dataset.position,
-    salary: Number(item.dataset.salary.replace(/[$,]/g, '')),
+    salary: getSalary(item),
     age: Number(item.dataset.age),
   }));
 }
 
-sortList(list);
-
-const employees = getEmployees(list);
-
-console.log(employees);
+sortList();
+getEmployees();
