cdc92be21f943a618ca937748ca32d8a79625cd0
Vlad Chudin <chudintttks@gmail.com>
2026-01-22 09:27:19 +0100

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalary(item) {
+  return Number(item.dataset.salary.replace(/[$,]/g, ''));
+}
+
+function sortList(list) {
+  const itemsArray = Array.from(list.children);
+
+  itemsArray.sort((a, b) => getSalary(a) - getSalary(b));
+
+  list.innerHTML = '';
+
+  itemsArray.forEach((item) => list.append(item));
+}
+
+function getEmployees(list) {
+  const itemsArray = Array.from(list.children);
+
+  return itemsArray.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: Number(item.dataset.salary.replace(/[$,]/g, '')),
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortList(list);
+
+const employees = getEmployees(list);
+
+console.log(employees);
